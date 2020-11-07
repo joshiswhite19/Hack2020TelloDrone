e9e52888841847302799f7ab0563714eb7bb9d54
Josh White <jowh8608@colorado.edu>
2020-11-07 10:45:43 -0700

    Added time logging

--- Hackathon Code/PatrolMain.py
+++ Hackathon Code/PatrolMain.py
@@ -5,83 +5,129 @@ sys.path.insert(0, parent_dir)
 from djitellopy import Tello
 import cv2
 import time
- 
+import math
+import numpy as np
+
 #################### DEFINE GLOBAL VARIABLES #########################
 X_BOUND = 4 * 30.48 # 4 feet in centimeters
 Y_BOUND = 6 * 30.48 # 6 feet in centimeters
+Z_BOUND = 8 * 30.48 # 8 feet in centimeters
 img_width = 320  # WIDTH OF THE IMAGE
 img_height = 240  # HEIGHT OF THE IMAGE
-simulation = 0   #  0 FOR FLIGHT 1 FOR TESTING
 state = {
-    "x": 0,
-    "y": 0,
-    "z": 0,
-    "theta": 0
+    'x': 0, # x position in cm
+    'y': 0, # y position in cm
+    'z': 0, # height in cm
+    'theta': 0 # Direction in radians
 }
+
 ######################################################################
  
+simulate = 1   #  0 FOR FLIGHT 1 FOR TESTING
+verbose = 1    #  1 FOR ADDED LOGGING, 0 OTHERWISE
+
 ################## CONNECT TO TELLO & INIT ###########################
-drone = Tello()
-drone.connect()
-drone.for_back_velocity = 0
-drone.left_right_velocity = 0
-drone.up_down_velocity = 0
-drone.yaw_velocity = 0
-drone.speed = 0
+if not simulate:
+    drone = Tello()
+    drone.connect()
+    drone.for_back_velocity = 0
+    drone.left_right_velocity = 0
+    drone.up_down_velocity = 0
+    drone.yaw_velocity = 0
+    drone.speed = 0
 ######################################################################
 
 ##################### COLLECT BASIC HEALTH INFO ######################
-print(drone.get_battery())
+    print(drone.get_battery())
 ######################################################################
 
-drone.streamoff()
-drone.streamon()
- 
-#################### IMPLEMENT MAIN FUNCTION #########################
-def main():
-    while True:
-    
-        # GET THE IMGAE FROM TELLO
-        frame_read = drone.get_frame_read()
-        myFrame = frame_read.frame
-        img = cv2.resize(myFrame, (img_width, img_height))
-    
-        # TO GO UP IN THE BEGINNING
-        if simulation == 0:
-            drone.takeoff()
-            time.sleep(8)
-            drone.rotate_clockwise(90)
-            time.sleep(3)
-            drone.move_left(35)
-            time.sleep(3)
-            drone.land()
-            startCounter = 1
-    
-        # # SEND VELOCITY VALUES TO TELLO
-        # if me.send_rc_control:
-        #     me.send_rc_control(me.left_right_velocity, me.for_back_velocity, me.up_down_velocity, me.yaw_velocity)
-    
-        # DISPLAY IMAGE
-        cv2.imshow("MyResult", img)
-    
-        # WAIT FOR THE 'Q' BUTTON TO STOP
-        if cv2.waitKey(33) == ord('a'):
-            drone.land()
-            break
-######################################################################
-if __name__ == "__main__":
-    main()
-######################################################################
+    drone.streamoff()
+    drone.streamon()
 
 ####################### AUXILIARY FUNCTIONS ##########################
-### DETERMINE PATROL PERIMETER FUNCTIONALITY
+### DETERMINE PATROL PERIMETER FUNCTIONALITY / MAIN MOVEMENT FUNCTION WRAPPERS
 
-def setState(state, x, y, z, theta):
-    state["x"] = x
+# Straight set of the state dictionary
+def setState(x, y, z, theta):
+    self.state["x"] = x
     state["y"] = y
     state["z"] = z
     state["theta"] = theta
+
+# Update the state dictionary
+def updateState(x, y, z, theta):
+    state['x'] += x
+    state['y'] += y
+    state['z'] += z
+    state['theta'] = (state['theta'] + theta) % (2 * np.pi)
+    if verbose: printState()
+
+def printState():
+    print(f'''
+    x = {state['x'] * (1/30.48)} ft
+    y = {state['y'] * (1/30.48)} ft
+    z = {state['z'] * (1/30.48)} ft
+    theta = {state['theta'] * (180/np.pi)} degrees
+    ''')
     
+# Wrapper for Tello.move_foward function
+def moveForward(x):
+    if not simulate: drone.move_forward(x)
+    updateState(
+        x * math.cos(state['theta']),
+        x * math.sin(state['theta']),
+        0, 0
+    )
+
+# Wrapper for Tello.move_back function
+def moveBack(x):
+    if not simulate: drone.move_back(x)
+    updateState(
+        -1 * x * math.cos(state['theta']),
+        -1 * x * math.sin(state['theta']),
+        0, 0
+    )
+
+# Wrapper for Tello.move_left function
+def moveLeft(x):
+    if not simulate: drone.move_left(x)
+    thet = state['theta'] + (np.pi/2)
+    updateState(
+        x*math.cos(thet),
+        x*math.sin(thet),
+        0, 0
+    )
+
+# Wrapper for Tello.move_right function
+def moveLeft(x):
+    if not simulate: drone.move_right(x)
+    thet = state['theta'] - (np.pi/2)
+    updateState(
+        x*math.cos(thet),
+        x*math.sin(thet),
+        0, 0
+    )
+
+# Wrapper for Tello.move_up function
+def moveUp(x):
+    if not simulate: drone.move_up(x)
+    updateState(0, 0, x, 0)
+
+# Wrapper for Tello.move_down function
+def moveDown(x):
+    if not simulate: drone.move_down(x)
+    updateState(0, 0, -1 * x, 0)
+
+# Wrapper for Tello.rotate_clockwise function
+def rotateCW(x):
+    if not simulate: drone.rotate_clockwise(x)
+    updateState(0, 0, 0, -1 * x * (np.pi/180))
+
+# Wrapper for Tello.rotate_counter_clockwise function
+def rotateCCW(x):
+    if not simulate: drone.rotate_counter_clockwise(x)
+    updateState(0, 0, 0, x * (np.pi/180))
+
 def checkBounds(state):
     lala = 1
 
@@ -97,4 +143,46 @@ def checkBounds(state):
 
 
 ### APPROPRIATE ACTION FUNCTIONALITY
+
+#################### IMPLEMENT MAIN FUNCTION #########################
+def main():
+    runtime = time.time()
+    while True:
+
+        if verbose: print(f'Current runtime: {time.time() - runtime} seconds')
+    # NEED TO REDO ALL OF THIS
+        # GET THE IMAGE FROM TELLO
+        #frame_read = drone.get_frame_read()
+        #myFrame = frame_read.frame
+        #img = cv2.resize(myFrame, (img_width, img_height))
+
+
+        if not simulate: drone.takeoff()
+        time.sleep(2)
+        rotateCW(90)
+        time.sleep(2)
+        moveLeft(35)
+        time.sleep(2)
+        if not simulate: drone.land()
+        startCounter = 1
+    
+        # # SEND VELOCITY VALUES TO TELLO
+        # if me.send_rc_control:
+        #     me.send_rc_control(me.left_right_velocity, me.for_back_velocity, me.up_down_velocity, me.yaw_velocity)
+    
+        # DISPLAY IMAGE
+        
+        #cv2.imshow("MyResult", img)
+    
+        
+######################################################################
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('\n KeyboardInterrupt recorded! Landing drone and quitting!')
+        if not simulate: drone.land()
+######################################################################
+
+
 
